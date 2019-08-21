dad9efe02b095f2e8b4a9df0735e8772010bb725
MayThinHan <maythinhan@mm.cybermissions.co.jp>
2019-08-21 12:22:12 +0900

    Create Attendance Table

---
+++ src/main/java/com/cmm/employee/entity/Attendance.java
@@ -0,0 +1,127 @@
+package com.cmm.employee.entity;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "attendance")
+public class Attendance {
+
+	@Id
+	@GeneratedValue
+	public Long id;
+
+	@Column(name = "employee_id")
+	public String employee_id;
+
+	@Column(name = "date")
+	public String date;
+
+	@Column(name = "day")
+	public String day;
+
+	@Column(name = "in_time")
+	public Time in_time;
+
+	@Column(name = "out_time")
+	public Time out_time;
+
+	@Column(name = "in_time_reason")
+	public String in_time_reason;
+
+	@Column(name = "out_time_reason")
+	public String out_time_reason;
+
+	@Column(name = "created_date")
+	public Timestamp created_date;
+
+	@Column(name = "updated_date")
+	public Timestamp updated_date;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(String employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public Time getIn_time() {
+		return in_time;
+	}
+
+	public void setIn_time(Time in_time) {
+		this.in_time = in_time;
+	}
+
+	public Time getOut_time() {
+		return out_time;
+	}
+
+	public void setOut_time(Time out_time) {
+		this.out_time = out_time;
+	}
+
+	public String getIn_time_reason() {
+		return in_time_reason;
+	}
+
+	public void setIn_time_reason(String in_time_reason) {
+		this.in_time_reason = in_time_reason;
+	}
+
+	public String getOut_time_reason() {
+		return out_time_reason;
+	}
+
+	public void setOut_time_reason(String out_time_reason) {
+		this.out_time_reason = out_time_reason;
+	}
+
+	public Timestamp getCreated_date() {
+		return created_date;
+	}
+
+	public void setCreated_date(Timestamp created_date) {
+		this.created_date = created_date;
+	}
+
+	public Timestamp getUpdated_date() {
+		return updated_date;
+	}
+
+	public void setUpdated_date(Timestamp updated_date) {
+		this.updated_date = updated_date;
+	}
+
+}
